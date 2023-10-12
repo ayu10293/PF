8aeb12b64da9b7daf85f655fda9c86b96e999c93
ayu10293 <snof11513@gmail.com>
2023-10-12 12:50:01 +0000

    ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  root to:'homes#top'
+  devise_for :customers, module: 'public', skip: [:passwords], contollers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admins,skip: [:registrations, :passwords], contllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    # Adminのルーティングを記述する
+  end
+
+  scope module: :public do
+    # カスタマーのルーティングを記述する
+    # ref: https://qiita.com/ryosuketter/items/9240d8c2561b5989f049#module--controller
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
