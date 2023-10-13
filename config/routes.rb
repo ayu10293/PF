c792bd6a46c51861a9d98091d59ed670da8afe55
ayu10293 <snof11513@gmail.com>
2023-10-13 13:05:56 +0000

    投稿から編集機能activestorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'recipes/new'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
