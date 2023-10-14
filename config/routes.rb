24600f094f7f817215a39e294be8dd2f80e05d27
ayu10293 <snof11513@gmail.com>
2023-10-14 09:38:23 +0000

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   get 'recipes/edit'
 
   root to:'homes#top'
+
   devise_for :customers, module: 'public', skip: [:passwords], contollers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
-  devise_for :admins,skip: [:registrations, :passwords], contllers: {
+  devise_for :admin, module: 'admin', skip: [:registrations, :passwords], contllers: {
     sessions: "admin/sessions"
   }
 
@@ -24,6 +25,8 @@ Rails.application.routes.draw do
     # カスタマーのルーティングを記述する
     # ref: https://qiita.com/ryosuketter/items/9240d8c2561b5989f049#module--controller
   end
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
