e10850e1e86b72fbe82a335da8f69cd413deb758
ayu10293 <snof11513@gmail.com>
2023-10-15 09:35:22 +0000

    mypage作成開始

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+Admin.create!(
+             email: "555@555",
+             password: "555555"
+             )
+
+Resipe.create!(
+    title: " 野菜炒め",
+    body: "おいしいよ"
+    )
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
