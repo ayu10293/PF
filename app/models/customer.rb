19ae910bbd8cfe75b78cc444e97c5d947f630323
ayu10293 <snof11513@gmail.com>
2023-10-17 09:18:30 +0000

    退会機能、フラッシュメッセージ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  
+  #同じアカウントでログイン不可能にする
+  #https://stackoverflow.com/questions/15865772/override-active-for-authentication-for-devise
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
