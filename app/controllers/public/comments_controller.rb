ba5e03e210cc9a63d71084e588e60b9429a76585
ayu10293 <snof11513@gmail.com>
2023-10-22 11:28:32 +0000

    エラーでまだ途中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,21 +1,21 @@
 class Public::CommentsController < ApplicationController
-  
+
   def create
     recipe = Recipe.find(params[:recipe_id])
-    comment = current_customer.comments.new(comment_params)
+    comment = current_customer.recipe_comments.new(recipe_comment_params)
     comment.recipe_id = recipe.id
     comment.save
     redirect_to recest.referer
   end
-  
+
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
   end
-  
+
   private
-  
-  def comment_params
+
+  def recipe_comment_params
     params.require(:recipe_comment).permit(:comment)
   end
 end
