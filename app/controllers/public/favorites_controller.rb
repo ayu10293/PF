788b39686f791c2fbb4fc8e62b88ecb764fb9b68
ayu10293 <snof11513@gmail.com>
2023-12-25 12:43:16 +0000

    非同期通信

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,16 +4,15 @@ class Public::FavoritesController < ApplicationController
   end
 
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    favorite = current_customer.favorites.new(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    #特定のユーザーがレシピをいいねしたときにfavoriteテーブルにデータを作る
+    favorite = current_customer.favorites.new(recipe_id: @recipe.id)
     favorite.save
-    redirect_to recipes_path
   end
 
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    favorite = current_customer.favorites.find_by(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    favorite = current_customer.favorites.find_by(recipe_id: @recipe.id)
     favorite.destroy
-    redirect_to recipes_path
   end
 end
